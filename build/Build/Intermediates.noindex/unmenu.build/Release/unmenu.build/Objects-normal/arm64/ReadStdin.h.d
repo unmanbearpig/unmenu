ce179ef4a6a2bfb88de9cbeb4f0ed8b1ca06ba3e
aspauldingcode <aspauldingcode@gmail.com>
2024-09-20 21:27:21 -0700

    No, Cargo.lock should not be ignored for libraries but should be committed for binaries. Since fuzzylib is a library, it should not be in .gitignore.

---
+++ build/Build/Intermediates.noindex/unmenu.build/Release/unmenu.build/Objects-normal/arm64/ReadStdin.h.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/Darwin.modulemap \
+  /Users/alex/unmenu/mac-app/src/ReadStdin.h.m \
+  /Users/alex/unmenu/mac-app/src/ReadStdin.h \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/Foundation.framework/Modules/module.modulemap
