928d7297e5ec4dc0cefc4b276d7cf0cfbf41bf66
aspauldingcode <aspauldingcode@gmail.com>
2024-09-25 11:24:44 -0600

    undo that

--- build/Build/Intermediates.noindex/unmenu.build/Release/unmenu.build/Script-7F2CCA26268F996200C14B29.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if which swiftlint >/dev/null; then
-  # swiftlint
-  echo "skipping swiftlint"
-else
-  echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
-fi
-
--- build/Build/Intermediates.noindex/unmenu.build/Release/unmenu.build/Script-7FA29F6D29A6139B00B27359.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-"${BUILT_PRODUCTS_DIR}/LaunchAtLogin_LaunchAtLogin.bundle/Contents/Resources/copy-helper-swiftpm.sh"
-
